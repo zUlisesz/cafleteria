4da61065cf144012b95bbe6ee152029e3a0f5903
zUlisesz <zperplexz@gmail.com>
2025-04-09 12:00:05 -0600

    Update main.py

--- main.py
+++ main.py
@@ -4,20 +4,146 @@ from pedido import Pedido
 
 def main(page: ft.Page) -> None:
     # General settings of the window
+    page.scroll = 'always'
     page.title = 'Main Window'
     page.vertical_alignment = ft.MainAxisAlignment.CENTER
-    page.theme_mode = ft.ThemeMode.DARK
+    page.theme_mode = ft.ThemeMode.LIGHT
     page.horizontal_alignment = ft.CrossAxisAlignment.CENTER
     
     # Elements of the GUI
-    administrador = ft.ElevatedButton(
-        text='ADMINISTRADOR', color=ft.Colors.BLACK, bgcolor=ft.Colors.BLUE_100, height=100, width=200
-    )
-    consumidor = ft.ElevatedButton(
-        text='CONSUMIDOR', color=ft.Colors.BLACK, bgcolor=ft.Colors.GREEN_100, height=100, width=200, on_click=lambda e: ConsumidorView(page)
+    img = ft.Image(
+        src=f"logoBuap.png",
+        width=600,
+        height=600,
+        fit=ft.ImageFit.CONTAIN,
     )
     
-    page.add(administrador, consumidor)
+    page.add(
+        ft.Text(value  = 'BIENVENIDO A LA CAFETERÍA BUAP', weight= ft.FontWeight.W_300,size=60 , color= ft.Colors.BLUE_GREY_700),
+        ft.Row(
+            spacing= 30 ,
+             
+            controls = [
+                ft.Container(
+                    content= img,
+                    width= 560,
+                    height= 560,
+                    alignment=ft.alignment.center
+                ),
+                ft.Container(
+                    content= ft.Column(
+                            spacing = 100,
+                            controls=[
+                                ft.Container(
+                                    padding  = 20,
+                                    width = 280 , 
+                                    height = 280 , 
+                                    alignment=ft.alignment.center,
+                                    content= ft.Image(
+                                        src=f"admin.png",
+                                        width=280,
+                                        height=280,
+                                        fit=ft.ImageFit.CONTAIN,
+                                    )
+                                ),
+                                ft.ElevatedButton(
+                                    on_click = lambda e: administradorView(page) ,
+                                    text='ADMINISTRADOR', 
+                                    color=ft.Colors.BLACK, 
+                                    bgcolor=ft.Colors.BLUE_GREY_200, 
+                                    height=100, 
+                                    width= 280
+                                )
+                            ]
+                        ),
+                
+                    width= 300,
+                    height= 560,
+                    padding = 20 , 
+                    alignment=ft.alignment.center
+                ),
+                ft.Container(
+                    content= ft.Column(
+                            spacing = 100,
+                            controls=[
+                                ft.Container(
+                                    padding = 20, 
+                                    width = 280 , 
+                                    height = 280 ,
+                                    alignment=ft.alignment.center, 
+                                    content= ft.Image(
+                                        src=f"customer.png",
+                                        width=280,
+                                        height=280,
+                                        fit=ft.ImageFit.CONTAIN,
+                                    ),
+                                ),
+                                ft.ElevatedButton(
+                                    text='CONSUMIDOR', 
+                                    color=ft.Colors.BLACK, 
+                                    bgcolor=ft.Colors.BLUE_GREY_200, 
+                                    height=100, width=280, 
+                                    on_click=lambda e: ConsumidorView(page)
+                                )
+                            ]
+                        ),
+                    width= 300,
+                    height= 560,
+                    padding = 20 , 
+                    alignment=ft.alignment.center,
+                )
+            ]
+        )
+    )
+    
+def administradorView(page: ft.Page)->None:
+    page.controls.clear()
+    page.horizontal_alignment = ft.CrossAxisAlignment.CENTER
+    page.vertical_alignment = ft.MainAxisAlignment.CENTER
+    
+    def validar(string )-> None:
+        codes = ['pass', 'password', 'unlocked','may i pass']
+        if string in codes :
+            page.clean()
+            page.add(
+                ft.Text(
+                    value = 'HAS ACCEDIO COMO ADMINISTRADOR',
+                    size= 40 ,
+                    weight = ft.FontWeight.BOLD
+                )
+            )
+        else:
+            letrero.value = '               CONTARASEÑA INCORRECTA'
+            password.value = ''
+            
+        page.update()
+    
+    password = ft.TextField(label = 'CONTRASEÑA', width = 300, password= True,border_radius= 8)
+    sign = ft.ElevatedButton( text= 'ACCEDER', width = 300, on_click= lambda e:validar( password.value))
+    letrero = ft.Text(value =  '', color=ft.Colors.RED_400, weight= ft.FontWeight.W_500)
+    
+    
+    page.add(
+        ft.Container(
+            bgcolor= ft.Colors.BLUE_GREY_200,
+            width= 500 ,
+            height= 300 ,
+            padding= 80 ,
+            border_radius= 8 ,
+            alignment= ft.alignment.center,
+            content= ft.Column(
+                spacing= 30 ,
+                controls=[
+                    ft.Text(value = 'ANTES DE ACCEDER INGRESA LA CONTRASEÑA', weight = ft.FontWeight.W_500),
+                    password,
+                    sign,
+                    letrero
+                ]
+            )
+            
+        )
+    )
+    page.update()
 
 def ConsumidorView(page: ft.Page) -> None:
     page.title = 'Creación de pedido'
